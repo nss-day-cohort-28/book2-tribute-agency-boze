ec9cf4f487bcc8ac4b3eaf118ba358ac472e4920
Zac Jones <zacjones91@gmail.com>
2018-10-09 15:13:19 -0500

    added array, randomizer function, alert with quotes

---
+++ script.js
@@ -0,0 +1,9 @@
+var quoteArray = [
+    "Kanye is cool", "Ye is the man", "NSS loves Kanye"
+];
+
+var quote = quoteArray[Math.floor(Math.random() * quoteArray.length)];
+
+console.log(quote);
+
+alert(quote);
